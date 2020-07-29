857328c28f853a86e11b272b74ecd6f69d0b7dee
Erick Alvarez Valencia <erickalv21@gmail.com>
2020-07-29 02:12:05 -0500

    Generating Card and Cardlist components to Monsters Rolodex App. Implementing search logic for monster cards.

---
+++ monsters-rolodex/build/precache-manifest.fca71ab74f318f9d7a075654b86aa078.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6feb84d370463d74704f37fffc00fdde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a33e52151f7c591109ee",
+    "url": "/static/css/main.28d0a32d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a33e52151f7c591109ee",
+    "url": "/static/js/main.4f225dae.chunk.js"
+  },
+  {
+    "revision": "4708b13b98379c58638d",
+    "url": "/static/js/runtime-main.03d56121.js"
+  }
+]);
