48998c27eea546cdf9a4934e9bea84582d96d1e5
Erick Alvarez Valencia <erickalv21@gmail.com>
2020-11-01 22:41:21 -0600

    Adding search functionality to App component and modifying page styles.

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
+
 import './App.css';
 
 class App extends Component {
@@ -18,6 +21,10 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -26,8 +33,12 @@ class App extends Component {
 
     return (
       <div className='App'>
-        <input type='search' placeholder='Search monsters' onChange={ e => this.setState({ searchField: e.target.value }) } />
-        <CardList monsters={ filteredMonsters } />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='Search monsters'
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
