b207d08ca7cb839e76cf8e1d8fe1acc8876a5465
Erick Alvarez <erickalv21@gmail.com>
2021-12-07 01:45:25 -0600

    Updating stripe integration and updating favicon and title

---
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51K3pOAFeSrKc82OiymXnWcdvNof6VyaIFJjzl5gQQ8TV4Wf6QlyZyz9Iv9ozThFw954vxgpRZIC7lo8sueIfqeLE00RBLiDn9k';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout 
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
